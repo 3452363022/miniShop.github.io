ea2684e458f69d7bb11c92b8c42b8ed35b8fd9e7
3452363022 <3452363022@qq.com>
2025-03-01 15:18:04 +0800

    Delete assets directory

--- assets/index-CsZPoOIW.js
+++
@@ -1,1 +0,0 @@
-import{q as t}from"./index-BWZonq4J.js";const r=()=>t.jsx("div",{children:"404"});export{r as default};
--- assets/index-DuqOv-Qj.js
+++
@@ -1,1 +0,0 @@
-import{q as a,bj as s}from"./index-BWZonq4J.js";const t=()=>a.jsx("div",{className:"cart",children:a.jsx(s,{})});export{t as default};
